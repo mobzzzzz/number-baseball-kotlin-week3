ca60968c54090fb954c57638d2c29756f4491c13
Jaewon Noh <opqw987@gmail.com>
2024-04-29 16:17:49 +0900

    Docs: 숫자 야구 게임 Javadoc 주석 추가

--- src/main/kotlin/NumberBaseballGame.kt
+++ src/main/kotlin/NumberBaseballGame.kt
@@ -9,20 +9,36 @@ class NumberBaseballGame {
         this.resetGame()
     }
 
+    /**
+     * 현재 게임 상태를 초기화합니다.
+     *
+     */
     fun resetGame() {
         this.resetAnswer()
         this.resetGameCount()
     }
 
+    /**
+     * 현재 게임 정답을 초기화합니다.
+     *
+     */
     private fun resetAnswer() {
         this.answer = "345"
     }
 
+    /**
+     * 숫자 야구 게임에 사용중인 볼 카운트를 초기화합니다.
+     */
     private fun resetGameCount() {
         this.strike = 0
         this.ball = 0
     }
 
+    /**
+     * 현재 게임 상태에 맞는 NumberBaseballGameStatus를 반환합니다.
+     *
+     * @return NumberBaseballGameStatus
+     */
     private fun validateAnswer(): NumberBaseballGameStatus {
         return when {
             this.strike == this.answer.length -> NumberBaseballGameStatus.Complete
@@ -31,6 +47,12 @@ class NumberBaseballGame {
         }
     }
 
+    /**
+     * 입력받은 값을 정답과 비교해 NumberBaseballGameStatus로 정답 여부를 반환합니다
+     *
+     * @param input: 입력받은 값
+     * @return NumberBaseballGameStatus: Nothing, Progress, Correct 세 가지 상태중 하나입니다.
+     */
     fun checkInput(input: String): NumberBaseballGameStatus {
         this.resetGameCount()
 
@@ -42,6 +64,11 @@ class NumberBaseballGame {
         return this.validateAnswer()
     }
 
+    /**
+     * 현재 스트라이크, 볼 카운트를 NumberBaseballCount로 묶어 completion으로 전달합니다.
+     *
+     * @param completion: Strike, Ball 정보를 가진 NumberBaseballCount를 전달합니다
+     */
     fun getCurrentGameCount(completion: (NumberBaseballCount) -> Unit) {
         completion(NumberBaseballCount(this.strike, this.ball))
     }
